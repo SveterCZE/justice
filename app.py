8c7044d9929589bc58a9f4e1a8018955f0fa2cbb
Petr Šmerkl <46304018+SveterCZE@users.noreply.github.com>
2021-05-02 14:20:01 +0200

    set debugging to false

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///justice.db'
-app.config["SQLALCHEMY_ECHO"] = True
-app.debug = True
+app.config["SQLALCHEMY_ECHO"] = False
+app.debug = False
 # HIDE THIS BEFORE DEPLOYING TO PRODUCTION :)
 app.secret_key = "123456"
 # toolbar = DebugToolbarExtension(app)
