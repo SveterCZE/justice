c5a41439b2033e08d136e0cfef8be02525706673
Petr Šmerkl <46304018+SveterCZE@users.noreply.github.com>
2021-05-10 23:37:27 +0200

    remove redundant sections

--- justice_build.py
+++ justice_build.py
@@ -4,16 +4,12 @@ from update_db import update_DB
 import os
 
 def main():
-    # typy_po = ["as", "sro", "vos", "ks", "dr", "zajzdrpo", "zahrfos", "ustav", "svj", "spolek", "prisp", "pobspolek",
-    #                 "oszpo", "osznadf", "osznad", "orgzam", "odbororg", "nadf", "nad", "evrspol", "evrhzs", "evrdrspol"]
-    # soudy = ["praha", "plzen", "brno", "ceske_budejovice", "hradec_kralove", "ostrava", "usti_nad_labem"]
-
     DB_name = "justice.db"
     create_DB(DB_name)
     valid_files = get_valid_filenames()
     os.makedirs("data", exist_ok=True)
-    # for valid_file in valid_files:
-    #     download_data(valid_file)
+    for valid_file in valid_files:
+        download_data(valid_file)
     for valid_file in valid_files:
         modified_file_name = os.path.join(str(os.getcwd()), "data", valid_file + ".xml")
         update_DB(modified_file_name, DB_name)
